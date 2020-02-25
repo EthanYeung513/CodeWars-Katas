64875665058b4121a88cd612920007dae2104408
EthanYeung513 <57107279+EthanYeung513@users.noreply.github.com>
2020-02-25 17:06:34 +0000

    New kata

---
+++ 8 kyu/Remove String Spaces.py
@@ -0,0 +1,9 @@
+#Kata name: Remove String Spaces
+
+#Kata link: https://www.codewars.com/kata/57eae20f5500ad98e50002c5/python
+
+#Kata description: Simple, remove the spaces from the string, then return the resultant string.
+
+
+def no_space(x):
+    return x.replace(' ' ,'')
