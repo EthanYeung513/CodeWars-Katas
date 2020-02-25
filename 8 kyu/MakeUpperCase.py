c36e4ff3c7393d0e312738fcaa7a3b2d672fa801
EthanYeung513 <57107279+EthanYeung513@users.noreply.github.com>
2020-02-25 14:03:22 +0000

    New kata

---
+++ 8 kyu/MakeUpperCase.py
@@ -0,0 +1,8 @@
+#Kata name: MakeUpperCase.
+
+#Kata link: https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+#Kata description: Write function makeUpperCase.
+
+def make_upper_case(s):
+    return s.upper()
