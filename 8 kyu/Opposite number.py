6a3d26ce4e99e772beb2f9c86db37cea42a580f8
EthanYeung513 <57107279+EthanYeung513@users.noreply.github.com>
2020-02-25 11:32:44 +0000

    New Kata

---
+++ 8 kyu/Opposite number.py
@@ -0,0 +1,10 @@
+#Kata name: Opposite number
+
+#Kata link: https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+#Kata description: Very simple, given a number, find its opposite.
+
+def opposite(number):
+  return number * -1  
+  #For example, (5 * -1) = -5 , (-8 * -1) = 8
+ 
