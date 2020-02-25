7d7d7059ceeea83370a71c3dde2df17709e9176f
EthanYeung513 <57107279+EthanYeung513@users.noreply.github.com>
2020-02-25 11:36:54 +0000

    New kata

---
+++ Convert a Number to a String!.py
@@ -0,0 +1,9 @@
+#Kata name: Convert a Number to a String!
+
+#Kata link: https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+
+#Kata Description: We need a function that can transform a number into a string.
+#What ways of achieving this do you know
+
+def number_to_string(num):
+    return str(num) #Typecasting to string
