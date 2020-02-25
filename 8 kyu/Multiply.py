33593fa81285126e245c8bbbacede969bcb694d8
EthanYeung513 <57107279+EthanYeung513@users.noreply.github.com>
2020-02-25 11:05:23 +0000

    New kata

---
+++ 8 kyu/Multiply.py
@@ -0,0 +1,6 @@
+#Kata name: Multiply
+#Kata link: https://www.codewars.com/kata/50654ddff44f800200000004
+#Kata description: The code does not execute properly. Try to figure out why.
+
+def multiply(a, b):
+  return a * b
