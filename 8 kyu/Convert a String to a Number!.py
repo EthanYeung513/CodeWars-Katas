92ffe920ea193d022bddc4a7aeaff81198aa4068
EthanYeung513 <57107279+EthanYeung513@users.noreply.github.com>
2020-02-25 14:47:57 +0000

    New kata

---
+++ 8 kyu/Convert a String to a Number!.py
@@ -0,0 +1,9 @@
+#Kata name: Convert a String to a Number!
+
+#Kata link: https://www.codewars.com/kata/544675c6f971f7399a000e79
+
+#Kata description: We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+
+def string_to_number(s):
+    return int(s) #Type casting from string to integer
