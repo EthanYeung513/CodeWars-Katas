6c327d2d0735d81e0b15f4347a201d16b09c6aae
EthanYeung513 <57107279+EthanYeung513@users.noreply.github.com>
2020-02-26 17:36:47 +0000

    New kata

---
+++ 8 kyu/Reversed Strings.py
@@ -0,0 +1,8 @@
+#Kata name: Reversed Strings
+
+#Kata link: https://www.codewars.com/kata/5168bb5dfe9a00b126000018/python
+
+#Kata description: Complete the solution so that it reverses the string value passed into it.
+
+def solution(string):
+    return string[::-1] 
