913eacc6a589319083aa311f51f7256fdb3e5495
EthanYeung513 <57107279+EthanYeung513@users.noreply.github.com>
2020-03-09 11:05:28 +0000

    New kata

---
+++ 8 kyu/Function 1 - hello world.py
@@ -0,0 +1,9 @@
+#Kata name: Function 1 - hello world
+
+#Kata link: https://www.codewars.com/kata/523b4ff7adca849afe000035
+
+#Kata description: Make a simple function called greet that returns the most-famous "hello world!".
+
+
+def greet():
+   return "hello world!"
