0ddbb3011fd06a96fdc91eeb96c5120f8e0fecf8
Ethan Yeung <57107279+EthanYeung513@users.noreply.github.com>
2020-05-29 20:55:56 +0100

    Update The Vowel Code.py

--- 6 kyu/The Vowel Code.py
+++ 6 kyu/The Vowel Code.py
@@ -1,6 +1,17 @@
 #Kata name: The Vowel Code
-#Kata link:
+#Kata link: https://www.codewars.com/kata/53697be005f803751e0015aa
 #Kata description: 
+#Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+
+#a -> 1
+#e -> 2
+#i -> 3
+#o -> 4
+#u -> 5
+
+#There is no need to worry about uppercase vowels in this kata.
+#Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+#You can assume that any numbers passed into the function will correspond to vowels.
 
 
 
